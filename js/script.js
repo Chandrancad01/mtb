b7f02479d0b86eb76240169c2a8efbda853711e1
chandrancad01 <Chandrancad01@gmail.com>
2024-01-21 22:54:08 +0530

    added files

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("welcome to javascript");
+console.log("welcome to javascript");
+console.log(10);
+console.log(10.10);
+
+//to print objects in console
+console.log({fname:"chandran", gender:"male"});
+console.table({fname:"chandran", gender:"male"});
+
+//to print array console
+console.log([5,3,20,23]);
+console.log([5,3,20,23]);
+console.log(true);  
+
+//print in console with variable decleration
+var a=5;
+let b=7;
+
+console.log(a);
+console.log(b);
